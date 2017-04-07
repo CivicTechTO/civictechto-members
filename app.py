886959910130474523906904e7c6fa05dc6f3a34
Patrick Connolly <patrick.c.connolly@gmail.com>
2017-04-06 23:47:53 -0400

    Migrated totally to meetup-api package. Push to ethercalc.

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import csv
+import io
 import meetup.api
 import os
 import requests
-import string
 import sys
 
 # Meetup attendance counts are no longer available via API
@@ -11,53 +11,58 @@ import sys
 MEETUP_API_KEY = os.environ['MEETUP_API_KEY']
 client = meetup.api.Client(MEETUP_API_KEY)
 
-base_url = 'https://api.meetup.com'
-data = {}
-data['urlname'] = 'Civic-Tech-Toronto'
-data['event_id'] = 238574683
-
-members_url = '/${urlname}/members'
-events_url  = '/${urlname}/events'
-rsvps_url   = '/${urlname}/events/${event_id}/rsvps'
-
-def build_url(endpoint, query_params={}):
-    url = base_url + string.Template(endpoint).substitute(**data)
-    url += '?key={}'.format(MEETUP_API_KEY)
-    url += '&page=200'
-    for k, v in query_params.items():
-        url += '&{}={}'.format(k, v)
-    return url
-
 members = []
 offset = 0
+count = 0
 while True:
     response = client.GetMembers({'group_urlname': 'Civic-Tech-Toronto', 'offset': offset})
     members.extend(response.results)
+    count += response.meta['count']
+    total = response.meta['total_count']
+    print('Members fetched: {}/{}'.format(count, total))
+
     if not response.meta['next']:
         break
-    per_page = response.meta['count']
-    total = response.meta['total_count']
-    print('Members fetched: {}/{}'.format(per_page*offset, total))
     offset += 1
 
 # Zero out attendance for each member
 for m in members:
-    m['attendance_count'] = 0
+    m.update({'attendance_count': 0})
 
-events = requests.get(build_url(events_url, {'status': 'past,upcoming'}))
-print(events.headers)
-events = events.json()
+count = 0
+response = client.GetEvents({'group_urlname': 'Civic-Tech-Toronto', 'status': 'past,upcoming'})
+events = response.results
+count += response.meta['count']
+total = response.meta['total_count']
+print('Events fetched: {}/{}'.format(count, total))
 event_ids = [e['id'] for e in events]
 
-for eid in event_ids:
-    data['event_id'] = eid
-    event_rsvps = requests.get(build_url(rsvps_url)).json()
-    attending_ids = [rsvp['member']['id'] for rsvp in event_rsvps]
-    for m in members:
-        if m['id'] in attending_ids:
-            m['attendance_count'] += 1
+rsvps = []
+for i, eid in enumerate(event_ids):
+    response = client.GetRsvps({'event_id': eid})
+    rsvps.extend(response.results)
+    print('Fetched {} RSVPs from event {}/{}'.format(response.meta['count'], i+1, len(event_ids)))
 
-writer = csv.writer(sys.stdout)
+for i, m in enumerate(members):
+    for r in rsvps:
+        if r['member']['member_id'] == m['id']:
+            m['attendance_count'] += 1
+    n = i + 1
+    if n % 100 == 0:
+        print('Processed RSVPs for member {}/{}'.format(n, len(members)))
+
+output['file'] = open('members.csv', 'w') # send to file
+output['screen'] = sys.stdout # send to screen
+output['ethercalc'] = io.StringIO() # send to ethercalc
+
+# Set output type here (hardcoded :/ )
+otype = 'ethercalc'
+writer = csv.writer(output[otype])
+
 writer.writerow(['name', 'meetup_member_id', 'meetup_attendance_count'])
 for m in members:
     writer.writerow([m['name'], m['id'], m['attendance_count']])
+
+if otype == 'ethercalc':
+    content = output.getvalue()
+    requests.put('https://ethercalc.org/_/civictechto-members', content.encode('utf-8'))
