85c04fafea90878a888c068b3816fe1742c0637f
Patrick Connolly <patrick.c.connolly@gmail.com>
2017-04-06 20:59:20 -0400

    Scrape all events, but only 200 members.

--- app.py
+++ app.py
@@ -18,25 +18,36 @@ members_url = '/${urlname}/members'
 events_url  = '/${urlname}/events'
 rsvps_url   = '/${urlname}/events/${event_id}/rsvps'
 
-def build_url(endpoint):
+def build_url(endpoint, query_params={}):
     url = base_url + string.Template(endpoint).substitute(**data)
     url += '?key={}'.format(MEETUP_API_KEY)
+    url += '&page=200'
+    for k, v in query_params.items():
+        url += '&{}={}'.format(k, v)
     return url
 
-meetup_members = requests.get(build_url(members_url)).json()
+meetup_members = requests.get(build_url(members_url))
+print(meetup_members.headers['X-Total-Count'])
+meetup_members = meetup_members.json()
 
 # Zero out attendance for each member
 for m in meetup_members:
     m['attendance_count'] = 0
 
-# Start with a single event, but we'll do this for all later
-one_event = requests.get(build_url(rsvps_url)).json()
-attending_ids = [rsvp['member']['id'] for rsvp in one_event]
-for m in meetup_members:
-    if m['id'] in attending_ids:
-        m['attendance_count'] += 1
+events = requests.get(build_url(events_url, {'status': 'past,upcoming'}))
+print(events.headers)
+events = events.json()
+event_ids = [e['id'] for e in events]
+
+for eid in event_ids:
+    data['event_id'] = eid
+    event_rsvps = requests.get(build_url(rsvps_url)).json()
+    attending_ids = [rsvp['member']['id'] for rsvp in event_rsvps]
+    for m in meetup_members:
+        if m['id'] in attending_ids:
+            m['attendance_count'] += 1
 
 writer = csv.writer(sys.stdout)
+writer.writerow(['name', 'meetup_member_id', 'meetup_attendance_count'])
 for m in meetup_members:
-    writer.writerow(['name', 'meetup_member_id', 'meetup_attendance_count'])
     writer.writerow([m['name'], m['id'], m['attendance_count']])
